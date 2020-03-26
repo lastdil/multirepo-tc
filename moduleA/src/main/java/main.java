76487e97ce8d4a024925f354b6b21a0b5d27a69d
Ivan Drygin <drygin@gmail.com>
2020-03-26 16:47:05 +0300

    test

--- moduleA/src/main/java/main.java
+++ moduleA/src/main/java/main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello from moduleA");
+        System.out.println("hello from moduleA   aaaa!");
     }
 }
