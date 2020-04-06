65e762d765444d074ad676b3da7cf7e38e69ebd9
Ivan Drygin <drygin@gmail.com>
2020-04-06 17:56:43 +0300

    test

--- moduleB/src/main/java/main.java
+++ moduleB/src/main/java/main.java
@@ -1,7 +1,13 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello from moduleB");
-        System.out.println("aloha from moduleB");
-        System.out.println("aloha from moduleB");
+//        System.out.println("hello from moduleB");
+//        System.out.println("aloha from moduleB");
+//        System.out.println("aloha from moduleB");
+
+
+        int x = 10;
+        int y = 2;
+
+        System.out.println(x);
     }
 }
